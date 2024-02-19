a9e0eeb897226d3e21fa7c2d9c0f4b1d37e736b2
DENNYYESSAR <dennisokeri5@gmail.com>
2024-02-19 14:14:04 -0800

    heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+void swap_elements(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+
+/**
+ * swap_elements - Swaps two elements in an array.
+ * @a: Pointer to the first element.
+ * @b: Pointer to the second element.
+ */
+void swap_elements(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * heapify_down - Maintains the max-heap property starting from a node downwards.
+ * @array: The array to heapify.
+ * @size: The size of the array.
+ * @base: The base index for the heapify operation.
+ * @root: The root index.
+ */
+void heapify_down(int *array, size_t size, size_t base, size_t root)
+{
+	size_t left, right, largest;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	largest = root;
+
+	if (left < base && array[left] > array[largest])
+		largest = left;
+	if (right < base && array[right] > array[largest])
+		largest = right;
+
+	if (largest != root)
+	{
+		swap_elements(&array[root], &array[largest]);
+		print_array(array, size);
+		heapify_down(array, size, base, largest);
+	}
+}
+
+/**
+ * heap_sort - Sorts an array of integers in ascending order using heap sort.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		heapify_down(array, size, size, i);
+
+	for (i = size - 1; i > 0; i--)
+	{
+		swap_elements(&array[0], &array[i]);
+		print_array(array, size);
+		heapify_down(array, size, i, 0);
+	}
+}
--- sort.h
+++ sort.h
@@ -2,6 +2,23 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /* Struct for doubly linked list node */
 typedef struct listint_s
@@ -22,5 +39,9 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
